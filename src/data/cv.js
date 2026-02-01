6fcedc18c0637c511c328d50482df6274d85fb15
Elisa Billard <elisabillard@gmail.com>
2026-02-01 12:15:42 +0000

    formatting updates + new CV

--- src/data/cv.js
+++ src/data/cv.js
@@ -5,7 +5,7 @@ export const cvData = {
   github: "https://github.com/elisabillard",
   tagline: "Biological Data Science | Master Student at EPFL",
   currentRole: "Research Student in Pietro Lio’s Computer Lab, Cambridge University",
-  thesis: "Deep learning for cancer diagnosis using spectral pathology (Infra-Red & Raman). Developing CycleGAN-based and U-Net virtual staining models to generate reliable histopathological (H&E) images. Supervised by Prof. Pietro Liò and Prof. Charlotte Bunne (EPFL)",
+  thesis: "Deep learning for cancer diagnosis using spectral pathology (Infra-Red & Raman). Developing CycleGAN-based and U-Net virtual staining models to generate reliable histopathological (H&E) images. Supervised by Prof. Pietro Liò and Prof. Charlotte Bunne (EPFL). Honoured by the Karl Zeno Schindler Foundation Grant.",
   
   experience: [
     { title: "Computational Biology Intern", company: "Orakl Oncology", logo: "/logos/orakloncology_logo.jpeg", location: "Paris, FR", date: "Feb 2025 - July 2025", desc: "Analyzed 150+ patient-derived organoids and improved biomarker identification pipeline (driver mutations, transcriptomic signatures) using WES and RNA-seq data." },
@@ -21,8 +21,8 @@ export const cvData = {
     logo: "/logos/epfl.png",  
     date: "2023 - 2025", 
     gpa: "5.5/6",
-    specialization: "Biological Data Science",
-    computational: ["Machine Learning (5/6)", "Applied Data Analysis (5.75/6)", "Digital Epidemiology", "Bioimage Informatics", "Biostatistics"],
+    specialization: "Specialised in Biological Data Science",
+    computational: ["Machine Learning (5/6)", "Applied Data Analysis (5.75/6)", "Digital Epidemiology", "Bioimage Informatics", "Biostatistics", "Genomics"],
     biology: ["Stem Cells", "Infection Biology", "Neurodegenerative Diseases", "Endocrinology"]
   },
   { 
@@ -31,8 +31,8 @@ export const cvData = {
     logo: "/logos/epfl.png", 
     date: "2019 - 2022", 
     gpa: "5.1/6",
-    computational: ["OOP", "Physics", "Algebra", "Statistics & Probability", "Machine Learning"],
-    biology: ["Synthetic Biology", "Immuno-engineering", "Oncology", "Genetics & Genomics"]
+    computational: ["Computer science", "Physics", "Algebra", "Statistics & Probability", "Machine Learning"],
+    biology: ["Synthetic Biology", "Immuno-engineering", "Oncology", "Neuroscience", "Genetics & Genomics", "Physiology"]
   },
   { 
     school: "LFZ", 
@@ -52,18 +52,19 @@ papers: [
     { 
       title: "Karl Zeno Schindler Foundation Research Grant", 
       year: "2025", 
-      note: "12,000 CHF to finance my research project on deep learning for Raman spectral pathology in the University of Cambridge, under supervision of Prof. Pietro Liò - Cambridge University and Prof. Charlotte Bunne - EPFL", 
+      note: "12k CHF to finance my research project on deep learning for Raman spectral pathology in the University of Cambridge, under supervision of Prof. Pietro Liò - Cambridge University and Prof. Charlotte Bunne - EPFL", 
     }
   ],
   projects: [
     { name: "Binder Protein Design Targeting PDGFR β", tools: "RFdiffusion, ProteinMPNN, AlphaFold", report: "Protein_design_project.pdf" },
     { name: "Image Based Cell Cycle Classification", tools: "CNN, Data Augementation, Transfer Learning", report: "ML_Project_2.pdf"},
     { name: "Gender Stereotypes in Movies", tools: "NLP, Clustering, Sentiment Analysis", link: "https://elisabillard.github.io/" },
+    { name: "Pollen Surveillance Digital Epidemiology Website", tools: "Streamlit, EpiCollect, Google API", report: "PollenPal_Report.pdf"},
     { name: "Hospital Patient Record Uniformization", tools: "Google Hackathon, Gemma" },
 
   ],
   skills: {
-    programming: ["Python (PyTorch, Scikit-learn)", "R", "C++", "Julia", "Java", "Git", "Docker"],
+    programming: ["Python", "PyTorch", "R", "C++", "Julia", "Java", "Git", "Docker"],
     languages: ["French Native", "German C1", "English C1", "Spanish B1"]
   }
 };
